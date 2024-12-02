5aef59afe4375d97ac76edc5fa9b77fbc7145c7b
Lee0701 <hgl1002@naver.com>
2024-12-02 22:23:08 +0900

    Add grouping and code types

--- cdl2sym.py
+++ cdl2sym.py
@@ -4,22 +4,42 @@ def main(args):
     input, output = args
     with open(input, 'rb') as f:
         data = list(f.read())
+    data = data[0x09:]
 
     result = []
 
-    for i, d in enumerate(data):
+    i = 0
+    while i < len(data):
         bank = i // 0x4000
         offset = i % 0x4000
         if bank != 0:
             offset += 0x4000
 
-        if d == 0x09:
-            type = 'code'
-        elif d == 0x01:
+        d = data[i]
+
+        if (d & 0x01) != 0x00:
+            if (d & 0x08) != 0x00:
+                result.append(f'{bank:02x}:{offset:04x} Call_{bank:02x}_{offset:04x}')
+            if (d & 0x04) != 0x00:
+                result.append(f'{bank:02x}:{offset:04x} Jump_{bank:02x}_{offset:04x}')
+            d = 0x01
+            data[i] = d
+
+        j = i
+        while data[j] == d:
+            if j + 1 == len(data):
+                break
+            if (j + 1) % 0x4000 == 0:
+                break
+            j += 1
+        length = j - i + 1
+        i += length
+
+        if d == 0x01:
             type = 'code'
         else:
             type = 'data'
-        length = 1
+
         result.append(f'{bank:02x}:{offset:04x} .{type}:{length:04x}')
 
     result = '\n'.join(result) + '\n'
